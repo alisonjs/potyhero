f48d4d593f9ece23c99af0641a4eeefb028ecb6b
Alison Sousa <alison.sousa@info.ufrn.br>
2020-03-25 16:59:28 -0300

    Create database and controllers

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const OngController = require('./controller/OngController');
+const IncidentController = require('./controller/IncidentController');
+const ProfileController = require('./controller/ProfileController');
 
 const routes = express.Router();
 
-routes.get('/',(request, response)=>{
-  return response.json({
-    user: 'alisonjs',
-    message: 'Teste Nodemon'
-  });
-});
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+routes.get('/incidents', IncidentsController.index);
+routes.post('/incidents', IncidentsController.create);
+routes.delete('/incidents', IncidentsController.delete);
+
+routes.get('/profiles', ProfileController.index);
+
+routes.post('/sessions', SessionController.create);
 
 module.exports = routes;
