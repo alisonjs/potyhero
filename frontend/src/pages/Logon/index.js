4724a764128a96d34bb4eab972fc4d3adc0627b6
Alison Silva <alison.jhonas15@gmail.com>
2020-03-29 16:00:27 -0300

    Create frontend pages NewIncidente and Profile

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,20 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { FiLogIn } from "react-icons/fi";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import api from "../../services/api";
 import "./styles.css";
+
 import logoImg from "../../assets/logo.svg";
 import heroesImg from "../../assets/heroes.png";
 
 export default function Logon() {
+  const [id, setId] = useState("");
+  const history = useHistory();
+
+  async function handleLogon(e) {
+    e.preventDefault();
+    try {
+      const response = await api.post("/sessions", { id });
+
+      localStorage.setItem("ongId", id);
+      localStorage.setItem("ongName", response.data.name);
+      history.push("/profile");
+    } catch (err) {
+      alert("Ocorreu um erro. Tente novamente.");
+    }
+  }
+
   return (
     <div className="logon-container">
       <section className="form">
         <img src={logoImg} alt="Poty Hero" />
 
-        <form action="">
+        <form onSubmit={handleLogon}>
           <h1>Faça seu Logon</h1>
 
-          <input placeholder="Sua ID" />
+          <input
+            placeholder="Sua ID"
+            value={id}
+            onChange={e => setId(e.target.value)}
+          />
           <button className="button" type="submit">
             Entrar
           </button>
